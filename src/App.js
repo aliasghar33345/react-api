9b3c70fb3720866e75e9d96e33e94daf3f54de33
Muhammad Ali Asghar <50847199+aliasghar33345@users.noreply.github.com>
2020-07-10 01:47:58 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useEffect, useState} from 'react';
+
 
 function App() {
+
+  const [data , setData] = useState([{}]); 
+   
+  useEffect(()=> {
+  
+    async function getrepos(){
+      const response = await fetch ("https://api.github.com/users/aliasghar33345/repos");
+      const data = await response.json();
+       console.log(data);
+       setData(data);
+    }
+
+    getrepos()
+    
+
+
+    // fetch('https://jsonplaceholder.typicode.com/posts/1')
+    // .then(response => response.json())
+    // .then(json => {
+    //      console.log(json);
+    //      setData(json);
+    // }
+    //   )
+  } , [] )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <ul>
+       {
+         data.map((datasObj , ind)=>{
+         return(<li key={ind}>{datasObj.name}</li>)
+         })
+       }
+     </ul>
     </div>
   );
 }
